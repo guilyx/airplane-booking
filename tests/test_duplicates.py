ca210cf1ac3aa9be5f8f4c88665db85cf389ebaf
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-02 21:10:51 +0100

    ✅ add dummy test

--- tests/test_duplicates.py
+++ tests/test_duplicates.py
@@ -3,3 +3,20 @@
 @date : 02/02/2020
 @brief : airplane reservation program
 '''
+
+
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from lib.passenger import Passenger
+from lib.booker import Booker
+from lib.flight import Flight
+
+def check_duplicate():
+    booker = Booker()
+    p1 = Passenger("Norbert", 101)
+    ticket = booker.generate_ticket(p1)
+    p1.n_tickets += 1
+
+    assert(len(booker.ticket_list) == 1)
+    
