0af0bc154d73b1a9e3d689c958080558d883bb8d
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-02 21:06:01 +0100

    ✨ add ticket generation

---
+++ lib/flight.py
@@ -0,0 +1,11 @@
+'''
+@author : Erwin Lejeune <erwin.lejeune15@gmail.com>
+@date : 02/02/2020
+@brief : airplane reservation program
+'''
+
+class Flight():
+    def __init__(self):
+        self.passenger_list = []
+
+    def set
