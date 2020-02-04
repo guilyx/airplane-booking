32bbcdd3400e65c5caba0bad6d811e7ae192aaf0
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-04 18:36:44 +0100

    🚀 Booking scenario complete

--- src/main.py
+++ src/main.py
@@ -15,19 +15,12 @@ import string, random
 
 
 def main():
-    maestro = Booker()
-    name_list = []
-    passports = []
-    passengers = []
-    for i in range(50):
-        name_list.append(''.join(random.choice(string.ascii_lowercase) for _ in range(7)))
-        passports.append(random.randint(1000, 10000))
-        passengers.append(Passenger(name_list[i], passports[i]))
+    destinations = ["Lyon", "Paris", "Marseille"]
+    maestro = Booker(destinations)
 
-    maestro.reserve_ticket(passengers[0])
+    while(maestro.exit == False):
+        maestro.main_menu()
 
-    print(maestro.flight_dtb["New York"].print_passenger())
-    print(passengers[0].print_info())
 
 
 
