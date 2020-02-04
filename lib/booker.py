3face2d96fc93e7a72e1dd015782c97364af211c
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-04 18:36:56 +0100

    🎨 improve display options

--- lib/booker.py
+++ lib/booker.py
@@ -2,11 +2,15 @@ import random
 import os
 from lib.flight import Flight
 from lib.passenger import Passenger
+import time
 
 
 class Booker():
-    def __init__(self):
-        self.destinations = ["New York", "Paris", "London", "Tokyo"]
+    def __init__(self, destinations_list = None):
+        if destinations_list == None:
+            self.destinations = ["New York", "Paris", "London", "Tokyo"]
+        else: 
+            self.destinations = destinations_list
         self.flight_dtb = dict()
 
         i = 0
@@ -14,42 +18,73 @@ class Booker():
             self.flight_dtb[elem] = Flight(elem)
             i += 1
 
+        self.passport_dtb = []
         self.passenger_dtb = dict()
-
         self.ticket_list = []
+
+        self.exit = False
 
     
     def main_menu(self):
         os.system('clear')
         print("---- MENU ----")
-        print("Hello, what do you want to do ?\n 1 - Reserve a Ticket\n 2 - Cancel a reservation\n 3 - Display ticket information.\n 4 - Exit")
+        known = False
+        name = input("Enter your name ---- ")
+        pass_n = input("Enter your passport number ---- ")
+        for items, keys in self.passenger_dtb.items():
+            if pass_n in items:
+                known = True
+            else:
+                known = False
+        if not(known):
+            self.passenger_dtb[pass_n] = Passenger(name, pass_n)
+        else:
+            print("Oh, you're already registered !")
+        self.current_user = self.passenger_dtb[pass_n]
+        print("Hello " + name + " what do you want to do ?\n 1 - Reserve a Ticket\n 2 - Cancel a reservation\n 3 - Display ticket information.\n 4 - Exit")
         opt = int(input("Choice ---- "))
-        return(opt)
+        
+        if(opt == 1):
+            self.reserve_ticket(self.current_user)
+        elif(opt == 4):
+            self.exit = True
+        else:
+            print("Not yet implemented.")
+
 
     
     def reserve_ticket(self, Passenger, rand_dest = False):
-        os.system('clear')
+        if (rand_dest == False):
+            os.system('clear')
         print("---- MENU ----")
-        print("Which destination ?\n 1 : New York\n 2 : Paris\n 3 : London\n 4 : Tokyo")
+        for i in range(len(self.destinations)):
+            print(str(i + 1) + " : " + self.destinations[i])
         if rand_dest == False:
             dest = 0
-            while dest < 1 or dest > 4:
+            while dest < 1 or dest > len(self.destinations):
                 dest = int(input("Choice : "))
         else:
-            dest = random.randint(1, 4)
+            dest = random.randint(1, len(self.destinations))
 
         chosen_flight = self.flight_dtb[self.destinations[dest - 1]]
+        
+        if(Passenger.passport_number in self.passport_dtb):
+            print("Your Passport Number is already used by someone, are you sure it is correct ?")
+        else:
+            self.passport_dtb.append(Passenger.passport_number)
         ticket = self.generate_ticket(Passenger)
         Passenger.book_ticket(chosen_flight, ticket)
         chosen_flight.add_passenger(Passenger)
-
+        print("Your ticket for " + chosen_flight.destination + " has been successfully booked.")
+        print("Returning to main menu...")
+        input("Press Enter to continue...")
+        time.sleep(2)
 
     def generate_ticket(self, Passenger):
         ticket = random.randint(1000, 100000)
         while(ticket in self.ticket_list):
             ticket = random.randint(1000, 100000)
         self.ticket_list.append(ticket)
-        self.passenger_dtb[ticket] = Passenger.pseudo
         return ticket
 
     
