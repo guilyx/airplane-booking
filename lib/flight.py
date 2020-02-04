66a5bb68fb336bc70a298e73e0054055a96e39f6
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-04 15:40:48 +0100

    ✨ add removal and addition of passenger

--- lib/flight.py
+++ lib/flight.py
@@ -4,7 +4,33 @@
 @brief : airplane reservation program
 '''
 
+from lib.passenger import Passenger
+import random
+from sys import stdout
+
 class Flight():
-    def __init__(self):
+    def __init__(self, destination):
         self.passenger_list = []
-        
+        self.max = random.randint(100, 350)
+        self.destination = destination
+
+    
+    def add_passenger(self, Passenger):
+        if (len(self.passenger_list) < self.max):
+            self.passenger_list.append(Passenger)
+
+
+    def remove_passenger(self, Passenger):
+        for elem in self.passenger_list:
+            if elem == Passenger:
+                self.passenger_list.remove(Passenger)
+            
+
+    def print_passenger(self):
+        i = 0
+        for elem in self.passenger_list:
+            stdout.write(elem.pseudo + " ; ")
+            if i > 20:
+                stdout.write('\n')
+                i = 0
+            i += 1
