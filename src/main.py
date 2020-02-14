9c686615dedb12abb38aadbf6a6f29ebf602865c
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-14 14:53:10 +0100

    🚧 WIP : GUI

--- src/main.py
+++ src/main.py
@@ -12,7 +12,9 @@ from os import system
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from lib.booker import Booker
 from lib.passenger import Passenger
+from lib.gui import Interface
 import string, random
+import tkinter as tk
 
 
 def main():
@@ -21,6 +23,13 @@ def main():
     while maestro.exit == False:
         maestro.main_menu()
 
+def frame():
+    window = tk.Tk()
+    frame = Interface(window)
+
+    frame.mainloop()
+    frame.destroy()
+
 
 if __name__ == "__main__":
-    main()
+    frame()
