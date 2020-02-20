62c5a4a847f19133ec52a780de90d06e4184fe84
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-02-20 17:27:08 +0100

    ⚡ add second part of the menu

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def main():
 def frame():
     window = tk.Tk()
     window.title("Booking Menu")
-    window.geometry('450x250')
+    window.geometry('800x300')
     window.config(background="light blue")
     frame = Interface(window)
 
